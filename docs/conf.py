149ef8c3ccb3cceb5df72ad8333d4ffc8c1a0f7c
Jeff Epler <jepler@gmail.com>
2024-07-16 15:37:30 -0500

    get readthedocs working again

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,13 @@ import sys
 
 sys.path.insert(0, os.path.abspath('.'))
 
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+html_context = {}
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    html_context["READTHEDOCS"] = True
 
 # -- Project information -----------------------------------------------------
 
