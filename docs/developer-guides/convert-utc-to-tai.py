e9ba7a4a614201c4d401c296260291b5d443240a
Jeff Epler <jepler@gmail.com>
2024-07-20 10:48:23 -0500

    datetime.UTC alias was new in 3.11

--- docs/developer-guides/convert-utc-to-tai.py
+++ docs/developer-guides/convert-utc-to-tai.py
@@ -1,8 +1,8 @@
-from datetime import UTC, datetime
+from datetime import timezone, datetime
 
 from leapseconddata import LeapSecondData
 
-my_date = datetime(2024, 7, 18, 22, 0, 0, tzinfo=UTC)
+my_date = datetime(2024, 7, 18, 22, 0, 0, tzinfo=timezone.utc)
 data = LeapSecondData.from_standard_source()
 
 my_tai_date = data.to_tai(my_date)
